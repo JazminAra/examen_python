bf752417b6cef173fcd69011a48dfec6659b13ec
JazminAra <windo205g@gmail.com>
2026-01-01 23:19:21 -0500

    feat:exercise

---
+++ README.md
@@ -0,0 +1,12 @@
+EJERCICIO 1 Revisión inicial del dataset
+
+Nro. de Filas: 167
+Nro. de Columnas: 6
+Costo de vida promedio: 82.92221556886227
+País con costo de vida más alto: Bermuda
+Costo de vida más alto: 225.86
+País con costo de vida más bajo: Egypt
+Costo de vida más bajo: 27.37
+Costo de Vida en Perú: 83.26
+Ranking de Perú: 60
+
---
+++ examen.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Cargar el dataset
+# ruta = "C:\\Users\\rcolr.BTS\\Documents\\CASA_INFORMATICA\\data\\living.csv"
+df = pd.read_csv('living.csv', encoding='latin1')
+# print(df.head())
+# print(df.info())
+# print(df.describe())
+# nuevo = pd.DataFrame(df)
+# print(nuevo)
+
+
+print("\nNro. de Filas:", df.shape[0])
+print("Nro. de Columnas:", df.shape[1])
+
+costos = df["Cost of living, 2017"].to_numpy()
+
+costo_promedio = np.mean(costos)
+costo_max = np.max(costos)
+costo_min = np.min(costos)
+
+print("Costo de vida promedio:", costo_promedio)
+
+idx_max = np.argmax(costos)
+print("País con costo de vida más alto:", df.loc[idx_max, "Countries"])
+print("Costo de vida más alto:", costo_max)
+
+
+idx_min = np.argmin(costos)
+print("País con costo de vida más bajo:", df.loc[idx_min, "Countries"])
+print("Costo de vida más bajo:", costo_min)
+
+
+peru = df[df["Countries"] == "Peru"]
+
+print("Costo de Vida en Perú:", peru["Cost of living, 2017"].to_numpy()[0])
+print("Ranking de Perú:", peru["Global rank"].to_numpy()[0])
+
+#  EJERCICIO 2
+# Los 10 países con el costo de vida más alto
+top_10_alto = df.sort_values(
+    by="Cost of living, 2017",
+    ascending=False
+).head(10)
+
+y_pos = np.arange(len(top_10_alto))
+
+plt.figure(figsize=(10, 6))
+plt.barh(
+    y_pos,
+    top_10_alto["Cost of living, 2017"].to_numpy()
+)
+plt.yticks(y_pos, top_10_alto["Countries"])
+plt.xlabel("Costo de vida (2017)")
+plt.ylabel("País")
+plt.title("Los 10 países con el costo de vida más alto")
+plt.gca().invert_yaxis()
+plt.show()
+
+#Los 10 países con el costo de vida más bajo
+
+top_10_bajo = df.sort_values(
+    by="Cost of living, 2017",
+    ascending=True
+).head(10)
+
+y_pos = np.arange(len(top_10_bajo))
+
+plt.figure(figsize=(10, 6))
+plt.barh(
+    y_pos,
+    top_10_bajo["Cost of living, 2017"].to_numpy()
+)
+plt.yticks(y_pos, top_10_bajo["Countries"])
+plt.xlabel("Costo de vida (2017)")
+plt.ylabel("País")
+plt.title("Los 10 países con el costo de vida más bajo")
+plt.gca().invert_yaxis()
+plt.show()
+
+
+# El costo de vida de los países de América
+
+america = df[df["Continent"] == "America"]
+
+x_pos = np.arange(len(america))
+
+plt.figure(figsize=(12, 6))
+plt.bar(
+    x_pos,
+    america["Cost of living, 2017"].to_numpy()
+)
+plt.xticks(x_pos, america["Countries"], rotation=90)
+plt.xlabel("País")
+plt.ylabel("Costo de vida (2017)")
+plt.title("El costo de vida de los países de América")
+plt.show()
